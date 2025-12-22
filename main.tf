69b56d223b05639902ee1f3b60d0c34af8de3c28
TheBigPieceOfChicken <241541599+TheBigPieceOfChicken@users.noreply.github.com>
2025-12-22 14:59:07 -0800

    Fix jamfpro_policy structure: Add payloads blocks with proper nesting

--- main.tf
+++ main.tf
@@ -23,29 +23,33 @@ resource "jamfpro_macos_configuration_profile_plist" "jamf_connect_login" {
 
 # Policy: Patch Jamf Connect Latest (ID: 49)
 resource "jamfpro_policy" "patch_jamf_connect" {
-  name                      = "Patch Jamf Connect Latest"
-  enabled                   = true
-  trigger_checkin           = true
-  trigger_enrollment_complete = false
-  trigger_login             = false
+  name                          = "Patch Jamf Connect Latest"
+  enabled                       = true
+  trigger_checkin               = true
+  trigger_enrollment_complete   = false
+  trigger_login                 = false
   trigger_network_state_changed = false
-  trigger_startup           = false
-  trigger_other             = ""
-  frequency                 = "Once every month"
-  retry_event               = "none"
-  retry_attempts            = -1
-  notify_on_each_failed_retry = false
-  target_drive              = "/"
-  offline                   = false
-  category_id               = -1  # Will be updated after category import
-  site_id                   = -1
+  trigger_startup               = false
+  trigger_other                 = ""
+  frequency                     = "Once every month"
+  retry_event                   = "none"
+  retry_attempts                = -1
+  notify_on_each_failed_retry   = false
+  target_drive                  = "/"
+  offline                       = false
+  category_id                   = -1 # Will be updated after category import
+  site_id                       = -1
 
-  package {
-    id                      = -1  # Reference to Jamf Connect package
-    action                  = "Install"
-    fill_user_template      = false
-    fill_existing_users     = false
-    update_autorun          = false
+  payloads {
+    packages {
+      package {
+        id                          = -1 # Reference to Jamf Connect package
+        action                      = "Install"
+        fill_user_template          = false
+        fill_existing_users         = false
+        update_autorun              = false
+      }
+    }
   }
 
   scope {
@@ -53,41 +57,49 @@ resource "jamfpro_policy" "patch_jamf_connect" {
     all_jss_users = false
   }
 
+  scope_limitations {
+    network_segment_ids = []
+  }
+
   scope_exclusions {
-    computer_group_ids = []  # Exclude "Enrolled Today" group
+    computer_group_ids = [] # Exclude "Enrolled Today" group
   }
 }
 
 # Policy: Start Jamf Connect Notify (ID: 33)
 resource "jamfpro_policy" "start_jamf_connect_notify" {
-  name                      = "00__Start Jamf Connect Notify"
-  enabled                   = true
-  trigger_checkin           = false
-  trigger_enrollment_complete = false
-  trigger_login             = false
+  name                          = "00__Start Jamf Connect Notify"
+  enabled                       = true
+  trigger_checkin               = false
+  trigger_enrollment_complete   = false
+  trigger_login                 = false
   trigger_network_state_changed = false
-  trigger_startup           = false
-  trigger_other             = "start-jcnotify"  # Custom event
-  frequency                 = "Ongoing"
-  retry_event               = "none"
-  retry_attempts            = -1
-  notify_on_each_failed_retry = false
-  target_drive              = "/"
-  offline                   = false
-  category_id               = -1  # Provisioning category
-  site_id                   = -1
+  trigger_startup               = false
+  trigger_other                 = "start-jcnotify" # Custom event
+  frequency                     = "Ongoing"
+  retry_event                   = "none"
+  retry_attempts                = -1
+  notify_on_each_failed_retry   = false
+  target_drive                  = "/"
+  offline                       = false
+  category_id                   = -1 # Provisioning category
+  site_id                       = -1
 
-  script {
-    id                      = -1  # Reference to Jamf Connect Notify script
-    priority                = "After"
-    parameter4              = ""
-    parameter5              = ""
-    parameter6              = ""
-    parameter7              = ""
-    parameter8              = ""
-    parameter9              = ""
-    parameter10             = ""
-    parameter11             = ""
+  payloads {
+    scripts {
+      script {
+        id         = -1 # Reference to Jamf Connect Notify script
+        priority   = "After"
+        parameter4 = ""
+        parameter5 = ""
+        parameter6 = ""
+        parameter7 = ""
+        parameter8 = ""
+        parameter9 = ""
+        parameter10 = ""
+        parameter11 = ""
+      }
+    }
   }
 
   scope {
