8a029c94ccf542017127a44db0b3925e19d3b02c
TheBigPieceOfChicken <241541599+TheBigPieceOfChicken@users.noreply.github.com>
2025-12-21 23:52:35 -0800

    Remove Google OAuth variables - use only Jamf Pro variables
    Removed google_oauth_client_id and google_oauth_client_secret variables since we're using Scalr's native secrets manager instead of GCP Secret Manager

--- variables.tf
+++ variables.tf
@@ -18,15 +18,3 @@ variable "jamf_client_secret" {
   type        = string
   sensitive   = true
 }
-
-variable "google_oauth_client_id" {
-  description = "Google OAuth Client ID for Jamf Connect"
-  type        = string
-  sensitive   = true
-}
-
-variable "google_oauth_client_secret" {
-  description = "Google OAuth Client Secret for Jamf Connect"
-  type        = string
-  sensitive   = true
-}
