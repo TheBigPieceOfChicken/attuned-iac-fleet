90412f13071c46204cc69e5489a9aae66accda7c
TheBigPieceOfChicken <241541599+TheBigPieceOfChicken@users.noreply.github.com>
2025-12-23 16:44:32 -0800

    Fix providers.tf syntax - upgrade to v0.30.0 for Self Service+ resources

--- providers.tf
+++ providers.tf
@@ -7,14 +7,15 @@ terraform {
   required_providers {
     jamfpro = {
       source  = "deploymenttheory/jamfpro"
-    version = "~> 0.30.0" 
+      version = "~> 0.30.0"
+    }
   }
 }
 
 # Jamf Pro Provider - uses secrets from Scalr variables
 provider "jamfpro" {
   jamfpro_instance_fqdn = var.jamf_url
-  client_id     = var.jamf_client_id
-  client_secret = var.jamf_client_secret
-  auth_method   = "oauth2"
+  client_id             = var.jamf_client_id
+  client_secret         = var.jamf_client_secret
+  auth_method           = "oauth2"
 }
