75f487818577741f00c3e4e7940a05a52abf2cea
TheBigPieceOfChicken <241541599+TheBigPieceOfChicken@users.noreply.github.com>
2025-12-23 23:56:16 -0800

    Fix syntax: Add newline after argument definition

--- main.tf
+++ main.tf
@@ -574,5 +574,6 @@ resource "jamfpro_computer_prestage_enrollment" "filevault_jamf_connect" {
     prefill_type                                 = "UNKNOWN"
     prefill_account_full_name                    = ""
     prefill_account_user_name                    = ""
-    prevent_prefill_info_from_modification = false}
+    prevent_prefill_info_from_modification = falselse
+}
 }
