a80740726900152489856f0a0b3883aeeefa8214
TheBigPieceOfChicken <241541599+TheBigPieceOfChicken@users.noreply.github.com>
2025-12-25 19:05:05 -0800

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -3,7 +3,6 @@
 
 terraform {
   required_version = ">= 1.0"
-
   required_providers {
     jamfpro = {
       source  = "deploymenttheory/jamfpro"
@@ -14,7 +13,7 @@ terraform {
 
 # Jamf Pro Provider - uses secrets from Scalr variables
 provider "jamfpro" {
-  jamfpro_instance_fqdn = var.jamf_url
+  jamfpro_instance_fqdn = var.jamf_instance_fqdn
   client_id             = var.jamf_client_id
   client_secret         = var.jamf_client_secret
   auth_method           = "oauth2"
