7a59ab37fac5d2e29c9109df4dfb363040ecfa3b
nick-pliancy <119696708+nick-pliancy@users.noreply.github.com>
2025-12-26 14:22:57 -0800

    update jamf connect license

--- jamf_connect_license.tf
+++ jamf_connect_license.tf
@@ -7,20 +7,11 @@ resource "jamfpro_macos_configuration_profile_plist" "jamf_connect_license" {
   payload_validate    = false
 
   payloads = templatefile("${path.module}/payloads/010-IDM-JamfConnect-License.plist.tpl", {
-    license_key     = var.client.jamf_connect.license_key
-    name            = var.client.name
-    email           = var.client.jamf_connect.email
-    date_issued     = var.client.jamf_connect.date_issued
-    expiration_date = var.client.jamf_connect.expiration_date
-    signature       = var.client.jamf_connect.signature
-    edition         = var.client.jamf_connect.edition
-    major_version   = var.client.jamf_connect.major_version
-    num_clients     = var.client.jamf_connect.num_clients
-    organization    = var.client.organization
+    license_file_base64 = var.jamf_connect_license_base64
+    organization        = var.client.organization
   })
 
   scope {
     all_computers = true
   }
 }
-
