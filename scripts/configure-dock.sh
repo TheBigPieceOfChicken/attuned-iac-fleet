0941a31a98cbbe4114a0d36bdb1ebe6e7641aed3
nick-pliancy <119696708+nick-pliancy@users.noreply.github.com>
2025-12-27 11:44:15 -0800

    Fix doctil by hardcoding ID

--- scripts/configure-dock.sh
+++ scripts/configure-dock.sh
@@ -1,41 +1,19 @@
 #!/bin/bash
+LoggedInUser=$( scutil <<< "show State:/Users/ConsoleUser" | awk '/Name :/ && ! /loginwindow/ { print $3 }' )
+LoggedInUserHome="/Users/$LoggedInUser"
 
-# Wait for dock to be available
-sleep 5
-
-# Get current logged in user
-currentUser=$( echo "show State:/Users/ConsoleUser" | scutil | awk '/Name :/ { print $3 }' )
-
-if [ "$currentUser" = "loginwindow" ] || [ -z "$currentUser" ]; then
-    echo "No user logged in, exiting"
-    exit 0
+if [ -e /usr/local/bin/dockutil ]; then
+    /usr/local/bin/dockutil --remove all --no-restart "$LoggedInUserHome"
+    /usr/local/bin/dockutil --add '/Applications/Google Chrome.app' --no-restart "$LoggedInUserHome"
+    /usr/local/bin/dockutil --add '/Applications/Slack.app' --no-restart "$LoggedInUserHome"
+    /usr/local/bin/dockutil --add '/Applications/zoom.us.app' --no-restart "$LoggedInUserHome"
+    # Add other apps as needed
+    /usr/local/bin/dockutil --add '/Applications' --view grid --display folder --no-restart "$LoggedInUserHome"
+    /usr/local/bin/dockutil --add '~/Downloads' --view fan --display stack --no-restart "$LoggedInUserHome"
+    
+    # Marker file for tracking
+    touch "$LoggedInUserHome/Library/Preferences/com.attuned.docksetup.plist"
 fi
 
-# Path to dockutil
-DOCKUTIL="/usr/local/bin/dockutil"
-
-# Remove unwanted default apps
-$DOCKUTIL --remove 'Maps' --no-restart "$currentUser"
-$DOCKUTIL --remove 'Pages' --no-restart "$currentUser"
-$DOCKUTIL --remove 'Numbers' --no-restart "$currentUser"
-$DOCKUTIL --remove 'Keynote' --no-restart "$currentUser"
-$DOCKUTIL --remove 'News' --no-restart "$currentUser"
-$DOCKUTIL --remove 'Stocks' --no-restart "$currentUser"
-$DOCKUTIL --remove 'TV' --no-restart "$currentUser"
-$DOCKUTIL --remove 'Music' --no-restart "$currentUser"
-$DOCKUTIL --remove 'Podcasts' --no-restart "$currentUser"
-$DOCKUTIL --remove 'Freeform' --no-restart "$currentUser"
-
-# Add apps you want
-$DOCKUTIL --add '/Applications/Google Chrome.app' --position 2 --no-restart "$currentUser"
-$DOCKUTIL --add '/Applications/Self Service+.app' --position end --no-restart "$currentUser"
-$DOCKUTIL --add '/Applications/Slack.app' --position 3 --no-restart "$currentUser"
-$DOCKUTIL --add '/Applications/Zoom.app' --position 4 --no-restart "$currentUser"
-$DOCKUTIL --add '/Applications/Google Drive.app' --position 5 --no-restart "$currentUser"
-$DOCKUTIL --add '/Applications/Keeper Password Manager.app' --position 6 --no-restart "$currentUser"
-
-# Restart dock to apply changes
-killall Dock
-
-echo "Dock configured successfully for $currentUser"
+killall -KILL Dock
 exit 0
